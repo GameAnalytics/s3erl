9c1f46b592f103efb0912ab8fc107be52ae6bd62
Sam Tavakoli <sam@tavakoli.se>
2018-03-23 17:29:58 +0000

    minor cleaned up

--- src/s3_app.erl
+++ src/s3_app.erl
@@ -17,26 +17,8 @@ stop() ->
 
 %%% Application callbacks
 
-
-%% @doc
-%% This function is called whenever an application is started using
-%% application:start/[1,2], and should start the processes of the
-%% application. Calls top level supervisor.
-%% @end
 start(_StartType, _StartArgs) ->
-    case s3_sup:start_link() of
-        {ok, Pid} ->
-            {ok, Pid};
-        Error ->
-            Error
-    end.
+    s3_sup:start_link().
 
-%% @doc
-%% This function is called whenever an application has stopped. It
-%% is intended to be the opposite of Module:start/2 and should do
-%% any necessary cleaning up. The return value is ignored.
-%% @end
 stop(_State) ->
     ok.
-
-%%% Internal functions
